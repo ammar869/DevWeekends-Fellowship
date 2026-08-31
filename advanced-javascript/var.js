0aac51101c4781d9a543c369b1641d9eeb7219d3
ammar869 <ammarhassan1131318@gmail.com>
2026-08-31 08:49:55 +0500

    Working with the promises

--- advanced-javascript/var.js
+++ advanced-javascript/var.js
@@ -1,5 +1,7 @@
 // var catName = "Oliver";
 // var catName = "Meow!";
 // console.log(catName);
-const FAV_PET = "Cats";
-FAV_PET = "Dogs";
+// const FAV_PET = "Cats";
+// FAV_PET = "Dogs";
+const sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+console.log(sampleStr);
