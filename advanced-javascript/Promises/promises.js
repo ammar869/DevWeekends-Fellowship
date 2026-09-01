e6b5c9a2f1f4533ab6fc6cccf0afb30d10d06748
ammar869 <ammarhassan1131318@gmail.com>
2026-09-01 08:59:19 +0500

    Promises

--- advanced-javascript/Promises/promises.js
+++ advanced-javascript/Promises/promises.js
@@ -1,4 +1,18 @@
-let promise = new Promise((resolve, reject) => {
-    console.log("Promise is pending");
-    resolve(123);
-});
+// let promise = new Promise((resolve, reject) => {
+//     console.log("Promise is pending");
+//     resolve(123);
+// });
+
+function getData(dataId, getNextData) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data", dataId);
+
+      if (getNextData) {
+        getNextData();
+      }
+
+      resolve("Success");
+    }, 5000);
+  });
+}
