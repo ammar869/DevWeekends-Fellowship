d21019013369c58b19b662a6d5b18491fc7fbe06
ammar869 <ammarhassan1131318@gmail.com>
2026-09-05 13:58:01 +0500

    Favicone handling

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -5,7 +5,9 @@ const url = require('url');
 const server = http.createServer((req, res) => {
     console.log("Server is running");
 
-
+    if(req.url === '/favicon.ico') {
+        console.log();
+    }
     const log = `${Date.now()}:${req.url} New Request recieved\n`
 
     // this code was used to handle different routes but we are not using it now because we are using the same server for all the requests
@@ -29,8 +31,8 @@ const server = http.createServer((req, res) => {
     fs.appendFile('log.txt', log , (err, data) => {
         console.log("Log is saved");
     });
-    console.log(req.headers);
-    console.log(req);
+    // console.log(req.headers);
+    // console.log(req);\
     res.end("OK Received the request");
 })
 // BUt we have  to make the port number dynamic so that we can run multiple servers on different ports
