0d5d0f755f5181b59423ef82a1de5da3dde38411
ammar869 <ammarhassan1131318@gmail.com>
2026-09-08 11:13:03 +0500

    Learned that It is not recommanded to put the servers in the same project

--- NodeJS/expresss.js
+++ NodeJS/expresss.js
@@ -2,3 +2,14 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const url = require('url');
+
+app.get("/",(req, res) => {
+    res.send("serving the home page");
+})
+app.get("/about",(req, res) => {
+    res.send("serving the about page");
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 000");
+})
